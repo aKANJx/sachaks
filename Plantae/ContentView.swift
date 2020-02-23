de66332fc3261e133cd241b4cbd27d53458f4970
aKANJ <me@akanj.com>
2020-02-23 01:37:19 -0800

    Initial Commit

---
+++ Plantae/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Plantae
+//
+//  Created by Jean Paul Marinho on 23/02/20.
+//  Copyright © 2020 aKANJ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
